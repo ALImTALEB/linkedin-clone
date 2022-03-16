4b3b6bbd956610b5753b47e7f80a5158c2c600ec
Ali Taleb <71498603+ALImTALEB@users.noreply.github.com>
2022-03-16 20:56:44 +0100

    header

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='header' >
+    fghgfh
+    
+
+  <div className="header__left">
+  <img src="" alt="" />
+  <div className="header__search">
+    <input type="text" />
+  </div>
+  </div>
+
+  <div className="header__right">
+
+  </div>
+    </div>
+  )
+}
+
+export default Header
