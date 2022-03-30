260ec42bc1774a119200d243fbe3766235085021
Ali Taleb <71498603+ALImTALEB@users.noreply.github.com>
2022-03-30 22:50:37 +0100

    header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import "./Header.css"
+import SearchIcon from '@material-ui/icons/Search'
 
 const Header = () => {
   return (
@@ -7,8 +9,10 @@ const Header = () => {
     
 
   <div className="header__left">
-  <img src="" alt="" />
+  <img src="https://cdn-icons.flaticon.com/png/512/3488/premium/3488338.png?token=exp=1648677821~hmac=7d402e8ad20baa187757d647855795de" 
+  alt="" />
   <div className="header__search">
+  <SearchIcon />
     <input type="text" />
   </div>
   </div>
